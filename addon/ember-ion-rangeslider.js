d0988bbfe2a3ca33e81d4646c514707dce6862d6
Vladimir Lascik <vlascik@users.noreply.github.com>
2017-04-05 06:24:54 +0200

    Do not use function prototype extensions

--- addon/ember-ion-rangeslider.js
+++ addon/ember-ion-rangeslider.js
@@ -10,18 +10,18 @@ export default Ember.Component.extend(IonSlider, {
   type: 'single', //## explicit, waiting for this.attr.type
   _slider: null,
 
-  sliderOptions: Ember.computed(function(){
+  sliderOptions: Ember.computed(function() {
     //## Update trigger: change|finish
     var updateTrigger = get(this, 'updateTrigger') || 'finish',
-        throttleTimeout = get(this, 'throttleTimeout') || 50,
-        to = get(this, 'to'),
-        from = get(this, 'from'),
-        options = {
-          to: 10,
-          from: 100,
-          onChange() {},
-          onFinish: Ember.run.bind(this, '_sliderDidFinish'),
-        };
+      throttleTimeout = get(this, 'throttleTimeout') || 50,
+      to = get(this, 'to'),
+      from = get(this, 'from'),
+      options = {
+        to: 10,
+        from: 100,
+        onChange() {},
+        onFinish: Ember.run.bind(this, '_sliderDidFinish'),
+      };
 
     if (from || from === 0) {
       options.from = from
@@ -39,42 +39,42 @@ export default Ember.Component.extend(IonSlider, {
   }).readOnly(),
 
   //## Setup/destroy
-  setupRangeSlider: function(){
+  didInsertElement() {
     var options = get(this, 'sliderOptions');
     this.$().ionRangeSlider(options);
     this._slider = this.$().data('ionRangeSlider');
+    this._startObserving();
+  },
 
-  }.on('didInsertElement'),
-
-  destroyRangeSlider: function(){
+  willDestroyElement() {
+    this._stopObserving();
     this._slider.destroy();
-
-  }.on('willDestroyElement'),
+  },
 
   //## Bound values observers
   _onToFromPropertiesChanged: Ember.observer(
     'to', 'from',
-    function(){
+    function() {
       var propName = arguments[1];
 
       //## slider.update removes the focus from the currently active element.
       //## In case where multiple sliders bound to the same property
       //## don't update the active slider values (to/from) as it results in a
       //## a loss of focus in a currently active slider
-      if(this._slider && !this._slider.is_active){
+      if (this._slider && !this._slider.is_active) {
         this._slider.update(this.getProperties(propName));
       }
-  }),
+    }),
 
-  _readOnlyPropertiesChanged: function(){
+  _readOnlyPropertiesChanged: function() {
     this._slider.update(this.getProperties(arguments[1]));
   },
 
-  _sliderDidChange: function(throttleTimeout, changes){
-    var args = {'to': changes.to, 'from': changes.from };
+  _sliderDidChange: function(throttleTimeout, changes) {
+    var args = { 'to': changes.to, 'from': changes.from };
     Ember.run.debounce(this, this.setProperties, args, throttleTimeout);
   },
-  _sliderDidFinish: function(changes){
-    this.setProperties({'to': changes.to, 'from': changes.from});
+  _sliderDidFinish: function(changes) {
+    this.setProperties({ 'to': changes.to, 'from': changes.from });
   },
 });
